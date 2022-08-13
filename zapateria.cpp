9a931c165e86cd16046493c82c40a009b31cd507
Bobacho <105739811+Bobacho@users.noreply.github.com>
2022-08-13 14:11:33 -0500

    #

--- zapateria.cpp
+++ zapateria.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int menu()
+{
+    int opc;
+    do{
+    cout << "[1]: Productos."<<endl;
+    cout << "[2]: Ventas."<< endl;
+    cout << "[3]: Datos de la empresa"<< endl;
+    cout << "[4]: Salir"<< endl;
+    cout << "Ingrese una opcion:[";cin>>opc;cout << "]";
+    }
+    while(opc<1 or opc>4);
+    return opc;
+}
+
 struct clientes
 {
     
